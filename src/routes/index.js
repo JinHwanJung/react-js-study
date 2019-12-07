091666b20ebe3109fa7aedcb8aa8fd844d715555
JungJinHwan <yadot1478@naver.com>
2019-12-07 22:10:41 +0900

    route index.js

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import User from './user';
+import Chat from './chat';
+import More from './more';
+
+
+export default [
+    { path: "/", component: User },
+    { path: "/caht", component: Chat },
+    { path: "/more", component: More }
+];
