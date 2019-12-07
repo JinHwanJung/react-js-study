83d57595c1100a5278c495593c25b3e346f2b56b
JungJinHwan <yadot1478@naver.com>
2019-12-07 22:14:09 +0900

    routing 기본 틀 작성

---
+++ src/routes/more.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const More = function() {
+    return <div>Chat</div>;
+};
+
+export default More;
---
+++ src/routes/user.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const User = function() {
+    return <div>Chat</div>;
+};
+
+export default User;
